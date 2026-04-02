d8f4c6c318f5c3b290ebca75b6cee4fd42cf7181
loris <loris.berti@gmail.com>
2026-04-02 02:28:57 +0200

    added timer lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,63 @@
+/*
+for pin configuration check:
+MAX7219_7Segment/max7219.h
+TFT_eSPI/User_setup.h
+*/
+
+/*
+TODO: the commented part is not making work the MAX7219 part
+*/
+
 #include <Arduino.h>
 #include "max7219.h"
+#include <SPI.h>
 #include <TFT_eSPI.h>
+#include <ESP32TimerInterrupt.h>
 
 MAX7219 display;
 TFT_eSPI tft = TFT_eSPI();
 
+ESP32Timer timer0(0);
+volatile bool timerTicked = false;
+volatile unsigned long timerCount = 0;
+char timerText[9] = "00000000";
+
+bool IRAM_ATTR onTimer(void *param)
+{
+  (void) param;
+  timerTicked = true;
+  timerCount++;
+  return true;
+}
+
 void setup() {
+
   display.Begin();
   display.Clear();
   display.MAX7219_SetBrightness(8);
-  display.DisplayText("01234567", 0, true);
+  display.DisplayText(timerText, 0, true);
 
-  tft.begin();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(2);
-  tft.drawString("ILI9225 OK", 10, 10, 2);
+  // tft.begin();
+  // tft.setRotation(1);
+  // tft.fillScreen(TFT_BLACK);
+  // tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  // tft.setTextSize(2);
+  // tft.drawString("ILI9225 OK", 10, 10, 2);
+  
+  if (!timer0.attachInterruptInterval(1000000, onTimer)) {
+    // Timer start failed
+    tft.drawString("Timer ERR", 10, 40, 2);
+  }
 
 }
 
 void loop() {
+  if (timerTicked) {
+    timerTicked = false;
+    snprintf(timerText, sizeof(timerText), "%08u", timerCount);
+    display.DisplayText(timerText, 0, true);
+    // tft.fillRect(10, 40, 220, 30, TFT_BLACK);
+    // tft.drawString(String("Timer: ") + timerText, 10, 40, 2);
+  }
 }
+
